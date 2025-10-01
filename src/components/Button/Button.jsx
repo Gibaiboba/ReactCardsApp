424a6c7fcdb3c7e339c40cbf15dd81eb6fa59260
gibaiboba <horshva@gmail.com>
2025-10-01 22:27:47 +0300

    header and button implementation

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,17 +1,11 @@
 import cls from "./Button.module.css";
 
-// const styleColor = {
-//   color: "yellow",
-//   backgroundColor: "#ccc",
-// };
-
-const isPrimary = true;
-
-export const Button = ({ onClick, children }) => {
+export const Button = ({ onClick, isActive, isDisabled, children }) => {
   return (
     <button
-      className={`${cls.btn} ${isPrimary ? cls.primary : ""}`}
+      className={`${cls.btn} ${isActive ? cls.active : ""}`}
       onClick={onClick}
+      disabled={isDisabled}
     >
       {children}
     </button>
