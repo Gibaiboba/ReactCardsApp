b906ff4bcc62f31d6b4725b95cbb77839f9b2955
gibaiboba <horshva@gmail.com>
2025-10-02 16:25:59 +0300

    not found page, question card props, dynamic route

--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -1,24 +1,24 @@
 import cls from "./QuestionCard.module.css";
 import { Button } from "../Button/Button";
+import { useNavigate } from "react-router-dom";
 
-export const QuestionCard = () => {
+export const QuestionCard = ({ card }) => {
+  const navigate = useNavigate();
+
   return (
     <div className={cls.card}>
       <div className={cls.cardLabels}>
-        <div>Level 1</div>
-        <div>Not completed</div>
+        <div>Level: {card.level} </div>
+        <div>{card.comleted ? "Completed" : "Not completed"}</div>
       </div>
 
-      <h5 className={cls.title}>What is JSX?</h5>
+      <h5 className={cls.title}>{card.question}</h5>
 
       <div className={cls.cardAnswers}>
         <label>Short answer:</label>
-        <p className={cls.cardAnswer}>
-          Lorem ipsum away doloy fuchen arsenskl gestuculate dkahalisi whaii
-          sunna cdirecti
-        </p>
+        <p className={cls.cardAnswer}>{card.answer}</p>
       </div>
-      <Button onClick={() => {}}>View</Button>
+      <Button onClick={() => navigate(`/question/${card.id}`)}>View</Button>
     </div>
   );
 };
---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,5 @@
+import cls from "./NotfoundPage.module.css";
+
+export const NotFoundPage = () => {
+  return <h2 className={cls.title}>Not Found Page</h2>;
+};
