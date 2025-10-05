1aa82183a5a0f3993b0722895af7a383186013aa
gibaiboba <horshva@gmail.com>
2025-10-06 00:20:27 +0300

    sort logic

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import cls from "./HomePage.module.css";
 import { API_URL } from "../../constans";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { QuestionCardList } from "../../components/QuestionCardList";
 import { Loader } from "../../components/Loader";
 import { useFetch } from "../../hooks/useFetch";
@@ -8,8 +8,8 @@ import { SearchInput } from "../../components/SearchInput";
 
 export const HomePage = () => {
   const [questions, setQuestions] = useState([]);
-
   const [searchValue, setSearchValue] = useState("");
+  const [sortSelectValue, setSortSelectValue] = useState("");
 
   const [getQuestion, isLoading, error] = useFetch(async (url) => {
     const response = await fetch(`${API_URL}/${url}`);
@@ -18,23 +18,47 @@ export const HomePage = () => {
     return questions;
   });
 
+  const cards = useMemo(() => {
+    return questions.filter((data) =>
+      data.question.toLowerCase().includes(searchValue.trim().toLowerCase())
+    );
+  }, [questions, searchValue]);
+
   useEffect(() => {
-    getQuestion("react");
-  }, []);
+    getQuestion(`react?${sortSelectValue}`);
+  }, [sortSelectValue]);
 
   const onSearchChangeHandler = (e) => {
-    console.log(e.target.value);
     setSearchValue(e.target.value);
+  };
+
+  const onSortSelectChangeHandler = (e) => {
+    setSortSelectValue(e.target.value);
   };
 
   return (
     <>
       <div className={cls.controlsContainer}>
         <SearchInput value={searchValue} onChange={onSearchChangeHandler} />
+
+        <select
+          value={sortSelectValue}
+          onChange={onSortSelectChangeHandler}
+          className={cls.select}
+        >
+          <option value="">Sort by</option>
+          <hr />
+          <option value="_sort=level">Level ASC</option>
+          <option value="_sort=-level">Level Desc</option>
+          <option value="_sort=completed">Completed ASC</option>
+          <option value="_sort=-completed">Completed Desc</option>
+        </select>
       </div>
+
       {isLoading && <Loader />}
       {error && <p>{error}</p>}
-      <QuestionCardList cards={questions} />
+      {cards.length === 0 && <p className={cls.noCardsInfo}>No cards...</p>}
+      <QuestionCardList cards={cards} />
     </>
   );
 };
