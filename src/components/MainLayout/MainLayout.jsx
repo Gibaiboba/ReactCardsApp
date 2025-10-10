f216cd1eef3203a9142b8a157b85ba193d5f015c
gibaiboba <horshva@gmail.com>
2025-10-10 23:47:19 +0300

    react toasts

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,23 +1,27 @@
 import { Outlet } from "react-router-dom";
 import cls from "./MainLayout.module.css";
 import { Header } from "../Header";
+import { ToastContainer } from "react-toastify";
 
 export const MainLayout = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className={cls.mainLayout}>
-      <Header />
-      <div className={cls.mainWrapper}>
-        <main className={cls.main}>
-          <Outlet />
-        </main>
-        <footer className={cls.footer}>
-          React Question Cards Application | {currentYear}
-          <br />
-          by Vitaly H.
-        </footer>
+    <>
+      <div className={cls.mainLayout}>
+        <Header />
+        <div className={cls.mainWrapper}>
+          <main className={cls.main}>
+            <Outlet />
+          </main>
+          <footer className={cls.footer}>
+            React Question Cards Application | {currentYear}
+            <br />
+            by Vitaly H.
+          </footer>
+        </div>
       </div>
-    </div>
+      <ToastContainer />
+    </>
   );
 };
