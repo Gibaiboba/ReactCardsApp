61074bfce589b20ea67fc2c9107aa3422cfb441b
gibaiboba <horshva@gmail.com>
2025-10-12 10:27:04 +0300

    add question page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MainLayout } from "./components/MainLayout";
 import { HomePage } from "./pages/HomePage";
-import { NotFoundPage } from "./pages/NotFoundPage/NotFoundPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 import { QuestionPage } from "./pages/QuestionPage";
+import { AddQuestionPage } from "./pages/AddQuestionPage";
 
 function App() {
   //   return <MainLayout />;
@@ -12,7 +13,7 @@ function App() {
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/forbidden" element={<div>forbidden</div>} />
-          <Route path="/addquestion" element={<div>addquestion</div>} />
+          <Route path="/addquestion" element={<AddQuestionPage />} />
           <Route path="/question/:id" element={<QuestionPage />} />
 
           <Route path="*" element={<NotFoundPage />} />
