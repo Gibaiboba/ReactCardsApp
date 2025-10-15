b869585f8515aa041499e4864ee1e1495bf2f8df
gibaiboba <horshva@gmail.com>
2025-10-16 00:17:24 +0300

    fix notfoundpage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,4 +1,4 @@
-import cls from "./NotfoundPage.module.css";
+import cls from "./NotFoundPage.module.css";
 
 export const NotFoundPage = () => {
   return <h2 className={cls.title}>Not Found Page</h2>;
